33119b80651e6d8e275ea297f53736fe9af4aa60
alireza <abiriali78@gmail.com>
2021-03-06 16:23:11 +0330

    Solved palindrome-easy question

---
+++ palindrome-easy.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("result: " + String.valueOf(solution.isPalindrome(1221)));
+        System.out.println("result: " + String.valueOf(solution.isPalindrome(1442323423)));
+    }
+
+    public boolean isPalindrome(int x) {
+        String inputString = String.valueOf(x);
+        boolean result = true;
+        if (x > Integer.MAX_VALUE || x < Integer.MIN_VALUE) {
+            return false;
+        }
+        for (int i = 0; i <= inputString.length() / 2; i++) {
+            if (inputString.charAt(i) != inputString.charAt(inputString.length() - 1 - i)) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
