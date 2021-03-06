bcb27696139340a75ced04f55b2b6d52e8fed612
alireza <abiriali78@gmail.com>
2021-03-06 16:42:38 +0330

    Fixed RomanToInt Issue Bug

--- RomanToInt.java
+++ RomanToInt.java
@@ -10,51 +10,57 @@ public class RomanToInt {
         for (int i = 0; i < s.length(); i++) {
             char currentChar = s.charAt(i);
             switch (currentChar) {
-                case 'I':
-                    if (s.charAt(i + 1) == 'V') {
-                        result += 4;
-                        i++;
-                    } else if (s.charAt(i + 1) == 'X') {
-                        result += 9;
-                        i++;
-                    } else {
-                        result += 1;
-                    }
-                    break;
-                case 'V':
-                    result += 5;
-                    break;
-                case 'L':
+            case 'I':
+                if (i == s.length() - 1) {
+                    result += 1;
+                } else if (s.charAt(i + 1) == 'V') {
+                    result += 4;
+                    i++;
+                } else if (s.charAt(i + 1) == 'X') {
+                    result += 9;
+                    i++;
+                } else {
+                    result += 1;
+                }
+                break;
+            case 'V':
+                result += 5;
+                break;
+            case 'L':
+                result += 50;
+                break;
+            case 'D':
+                result += 500;
+                break;
+            case 'M':
+                result += 1000;
+                break;
+            case 'X':
+                if (i == s.length() - 1) {
                     result += 50;
-                    break;
-                case 'D':
-                    result += 500;
-                    break;
-                case 'M':
-                    result += 1000;
-                    break;
-                case 'X':
-                    if (s.charAt(i + 1) == 'L') {
-                        result += 40;
-                        i++;
-                    } else if (s.charAt(i + 1) == 'C') {
-                        result += 90;
-                        i++;
-                    } else {
-                        result += 50;
-                    }
-                    break;
-                case 'C':
-                    if (s.charAt(i + 1) == 'D') {
-                        result += 400;
-                        i++;
-                    } else if (s.charAt(i + 1) == 'M') {
-                        result += 900;
-                        i++;
-                    } else {
-                        result += 100;
-                    }
-                    break;
+                } else if (s.charAt(i + 1) == 'L') {
+                    result += 40;
+                    i++;
+                } else if (s.charAt(i + 1) == 'C') {
+                    result += 90;
+                    i++;
+                } else {
+                    result += 50;
+                }
+                break;
+            case 'C':
+                if (i == s.length() - 1) {
+                    result += 100;
+                } else if (s.charAt(i + 1) == 'D') {
+                    result += 400;
+                    i++;
+                } else if (s.charAt(i + 1) == 'M') {
+                    result += 900;
+                    i++;
+                } else {
+                    result += 100;
+                }
+                break;
             }
         }
 
